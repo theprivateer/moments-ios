17bb3b618c638244edcf69702598128e9e00b296
Phil Stephens <hello@philstephens.com>
2026-03-02 12:51:31 +1000

    Initial Commit

---
+++ Moments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moments
+//
+//  Created by Phil Stephens on 2/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moments/MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentsApp.swift
+//  Moments
+//
+//  Created by Phil Stephens on 2/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
